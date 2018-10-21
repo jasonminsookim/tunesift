3d116ce75f6f9b03abefbaa97b3a256b5539c545
Jason Kim <jasonkim@Jasons-MacBook-Pro-6.local>
2018-10-21 14:24:40 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Music Review Aggregator and Predictor',
+    author='Jason Minsoo Kim',
+    license='MIT',
+)
